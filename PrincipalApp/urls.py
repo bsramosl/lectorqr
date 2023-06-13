74e96969fbb5d1774349f494c945daccc009d15c
Lopez <steeven10f@gmail.com>
2023-06-12 20:13:38 -0500

    2

--- PrincipalApp/urls.py
+++ PrincipalApp/urls.py
@@ -7,6 +7,6 @@ from django.contrib.auth.decorators import login_required
 app_name = 'Qr'
 urlpatterns = [  
      path('Index/',views.Index,name='Inicio'),    
-     path('Inicio/',views.Inicio.as_view(),name='Inicio'),    
+     path('',views.Inicio.as_view(),name='Inicio'),    
    
     ]
